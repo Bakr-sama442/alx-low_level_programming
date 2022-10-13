d87301ba4b7ba04cffcdef365231c2309d173d59
Bakr-sama442 <abubakarsadiqabubakar751@gmail.com>
2022-10-13 12:32:34 -0700

    my commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-enrty point for the program.
+ *
+ * Returns:0 if no errors  otherwise returns non-zero.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
