7f97b78e104fa894174aa75cd7cba8ab533d1a5f
Bakr-sama442 <abubakarsadiqabubakar751@gmail.com>
2022-10-18 15:04:20 -0700

    adds two integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#innclude "main.h"
+
+/**
+ * add - entry point
+ * Description: adds two integer
+ * Returns: sum
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
